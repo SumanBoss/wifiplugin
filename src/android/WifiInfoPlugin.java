3041831c9ed778cacef60fe49a737775d100fee0
Suman Jana <suman.jana2@gmail.com>
2013-11-13 15:21:54 +0530

    Update WifiInfoPlugin.java

--- src/android/WifiInfoPlugin.java
+++ src/android/WifiInfoPlugin.java
@@ -1,7 +1,8 @@
 package org.apache.cordova.plugin;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Context;
