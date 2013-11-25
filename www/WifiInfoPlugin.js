d0460284d760ab140f2bd59a702a32c3ed6095df
Suman Jana <suman.jana2@gmail.com>
2013-11-25 13:13:59 +0530

    Update WifiInfoPlugin.js

--- www/WifiInfoPlugin.js
+++ www/WifiInfoPlugin.js
@@ -1,8 +1,13 @@
-var utils = require('cordova/utils'),
-        exec = require('cordova/exec'),
-        cordova = require('cordova');
 
-WifiInfoPlugin.prototype.managewifi = function(success, fail) {
-    exec(success, fail, 'WifiInfoPlugin', 'managewifi', []);
+cordova.define('cordova/WifiInfoPlugin', function(require, exports, module) {
+    var exec = require('cordova/exec');
+
+    var MyOwnPlugin = function() {};
+
+    MyOwnPlugin.prototype.managewifi = function(onSuccess, onFailed) {
+       exec(success, fail, 'WifiInfoPlugin', 'managewifi', []);
     alert('here');
-};
+    }
+
+    module.exports = new MyOwnPlugin();
+});
