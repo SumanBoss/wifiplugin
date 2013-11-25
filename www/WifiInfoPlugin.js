9d0f0be1d7735b639a3d72e7ae0a5ca1ffc9a442
Suman Jana <suman.jana2@gmail.com>
2013-11-25 18:40:24 +0530

    Update WifiInfoPlugin.js

--- www/WifiInfoPlugin.js
+++ www/WifiInfoPlugin.js
@@ -2,7 +2,7 @@
 
     var MyOwnPlugin = function() {};
 
-    MyOwnPlugin.prototype.managewifi = function() {
+    MyOwnPlugin.prototype.managewifi = function(suc,fa) {
        exec(function(winParam) {
             alert('sucess plug');
        }, function(error) {
