cad7928adec9a402be5d319d0b16098814dfdc99
Suman Jana <suman.jana2@gmail.com>
2013-11-29 12:44:25 +0530

    Update WifiInfoPlugin.js

--- www/WifiInfoPlugin.js
+++ www/WifiInfoPlugin.js
@@ -2,12 +2,15 @@
 
     var MyOwnPlugin = function() {};
 
-  MyOwnPlugin.prototype.managewifi = function(successCallback, errorCallback) {
+ MyOwnPlugin.prototype.managewifi = function(successCallback, errorCallback) {
        exec(successCallback, errorCallback, 'wifi', 'managewifi', []);
-    }
+    };
     
       MyOwnPlugin.prototype.getdevice = function(successCallback, errorCallback) {
        exec(successCallback, errorCallback, 'wifi', 'getdevice', []);
-    }
+    };
+      MyOwnPlugin.prototype.info = function(successCallback, errorCallback) {
+       exec(successCallback, errorCallback, 'wifi', 'wifinfo', []);
+    };
     module.exports = new MyOwnPlugin();
 
